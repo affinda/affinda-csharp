c24881028015a84e054e2420be302071494d47b6
rockstarr-programmerr <rockstarrprogrammerr@gmail.com>
2023-09-21 03:47:49 +0000

    #autocommit #update #csharp - add document.rejected webhook event

--- AffindaAPI/AffindaAPI/AffindaAPIRestClient.cs
+++ AffindaAPI/AffindaAPI/AffindaAPIRestClient.cs
@@ -6063,10 +6063,10 @@ namespace Affinda.API
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
         /// <remarks>
-        /// After a subscription is sucessfully created, we&apos;ll send a POST request to your target URL with a `X-Hook-Secret` header. &lt;br /&gt;
+        /// After a subscription is successfully created, we&apos;ll send a POST request to your target URL with a `X-Hook-Secret` header. &lt;br /&gt;
         /// You need to response to this request with a 200 status code to confirm your subscribe intention. &lt;br /&gt;
         /// Then, you need to use the `X-Hook-Secret` to activate the subscription using the [/resthook_subscriptions/activate](#post-/v3/resthook_subscriptions/activate) endpoint. &lt;br /&gt;
-        /// For more information, see our help article here - [How do I create a webhook?](https://help.affinda.com/hc/en-au/articles/11474095148569-How-do-I-create-a-webhook)
+        /// For more information and detailed instructions, [see our webhooks documentation here.](https://docs.affinda.com/docs/webhooks)
         /// </remarks>
         public async Task<Response<ResthookSubscription>> CreateResthookSubscriptionAsync(ResthookSubscriptionCreate body, CancellationToken cancellationToken = default)
         {
@@ -6096,10 +6096,10 @@ namespace Affinda.API
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
         /// <remarks>
-        /// After a subscription is sucessfully created, we&apos;ll send a POST request to your target URL with a `X-Hook-Secret` header. &lt;br /&gt;
+        /// After a subscription is successfully created, we&apos;ll send a POST request to your target URL with a `X-Hook-Secret` header. &lt;br /&gt;
         /// You need to response to this request with a 200 status code to confirm your subscribe intention. &lt;br /&gt;
         /// Then, you need to use the `X-Hook-Secret` to activate the subscription using the [/resthook_subscriptions/activate](#post-/v3/resthook_subscriptions/activate) endpoint. &lt;br /&gt;
-        /// For more information, see our help article here - [How do I create a webhook?](https://help.affinda.com/hc/en-au/articles/11474095148569-How-do-I-create-a-webhook)
+        /// For more information and detailed instructions, [see our webhooks documentation here.](https://docs.affinda.com/docs/webhooks)
         /// </remarks>
         public Response<ResthookSubscription> CreateResthookSubscription(ResthookSubscriptionCreate body, CancellationToken cancellationToken = default)
         {
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Added
 - Allow creating workspace-scope webhook
+- Add "document.rejected" webhook event
 
 ## [5.10.0] - 2023-07-26
 ### Changed
